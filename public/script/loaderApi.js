893021bc9ddd9c1ec8266f9e19b7fef2de2728b9
Philipp Hermes <philipphermes2001@gmail.com>
2022-07-09 12:37:40 +0200

    add icon

--- public/script/loaderApi.js
+++ public/script/loaderApi.js
@@ -4,5 +4,5 @@ function download() {
     let link = document.getElementById('link').value;
     let format = document.getElementById('format').value;
 
-    output.innerHTML = '<iframe sandbox="allow-same-origin allow-scripts allow-downloads" style="height:50px; border:none; overflow:hidden;" src="https://loader.to/api/button/?url=' + link + '&f=' + format + '"></iframe>';
+    output.innerHTML = '<iframe sandbox="allow-same-origin allow-scripts allow-downloads" src="https://loader.to/api/button/?url=' + link + '&f=' + format + '"></iframe>';
 }
